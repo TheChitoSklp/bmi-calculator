a93c91d5116ce52ea72becc3edb430c749386f13
chito <chito.pepsi@gmail.com>
2024-01-30 10:18:25 -0600

    new styles

--- src/components/InputImperial.jsx
+++ src/components/InputImperial.jsx
@@ -1,3 +1,4 @@
+import "./InputImperial.css";
 export const InputImperial = ({
   heightFT,
   heightIN,
@@ -13,13 +14,25 @@ export const InputImperial = ({
       <div>
         <label>
           Height:
-          <div className="input-with-unit">
-            <input type="text" value={heightFT} onChange={handleHeightFTChange} />
-            <span className="unit">ft</span>
-          </div>
-          <div className="input-with-unit">
-            <input type="text" value={heightIN} onChange={handleHeightINChange} />
-            <span className="unit">in</span>
+          <div className="input-pair-container">
+            <div className="input-with-unit">
+              <input
+                className="input-estilizado"
+                type="text"
+                value={heightFT}
+                onChange={handleHeightFTChange}
+              />
+              <span className="unit">ft</span>
+            </div>
+            <div className="input-with-unit">
+              <input
+                className="input-estilizado"
+                type="text"
+                value={heightIN}
+                onChange={handleHeightINChange}
+              />
+              <span className="unit">in</span>
+            </div>
           </div>
         </label>
       </div>
@@ -27,13 +40,25 @@ export const InputImperial = ({
       <div>
         <label>
           Weight:
-          <div className="input-with-unit">
-            <input type="text" value={weightST} onChange={handleWeightSTChange} />
-            <span className="unit">st</span>
-          </div>
-          <div className="input-with-unit">
-            <input type="text" value={weightLBS} onChange={handleWeightLBSChange} />
-            <span className="unit">lbs</span>
+          <div className="input-pair-container">
+            <div className="input-with-unit">
+              <input
+                className="input-estilizado"
+                type="text"
+                value={weightST}
+                onChange={handleWeightSTChange}
+              />
+              <span className="unit">st</span>
+            </div>
+            <div className="input-with-unit">
+              <input
+                className="input-estilizado"
+                type="text"
+                value={weightLBS}
+                onChange={handleWeightLBSChange}
+              />
+              <span className="unit">lbs</span>
+            </div>
           </div>
         </label>
       </div>
